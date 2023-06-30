e6b5f87b0e9ac573d0296c2c7dc1dbb176ec1512
udaysk3 <burluudaysantoshkumar3@gmail.com>
2023-07-01 01:52:01 +0530

    Deployment settings

--- CardGenerator/settings.py
+++ CardGenerator/settings.py
@@ -142,9 +142,9 @@ AUTH_USER_MODEL = "user.User"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 import os
-STATIC_URL = '/static/'
+STATIC_URL = 'staticfiles/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
-# STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # PAYPAL_RECEIVER_EMAIL = 'udaysk963@gmail.com'
